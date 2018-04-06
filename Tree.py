2c167037daca0d395d4cc726b7e5eb4bb5aaf8d9
idgd <isaac.dudney@icloud.com>
2018-04-06 11:44:45 -0600

    added potentially functional search func

--- Tree.py
+++ Tree.py
@@ -5,11 +5,22 @@ class Tree:
 		self.root = Node(root)
 
 	def add(self,node,parent):
-		if search(parent):
+		if search(parent,self):
 			return(True)
 		else:
 			print("error: parent not found")
 			return(False)
 
-	def search(self,node):
-		return()
+	def search(self,node,tree):
+		n = tree.root
+
+		if n.data == node:
+			return(True)
+
+		while n.relatives:
+			if node in n.relatives:
+				return(True)
+			for f in n.relatives:
+				search(node,f)
+
+		return(False)
