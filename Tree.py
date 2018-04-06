a5faf3c353f70b5e912a467295e7a0ca32e9070a
idgd <isaac.dudney@icloud.com>
2018-04-06 12:16:29 -0600

    fixed search

--- Tree.py
+++ Tree.py
@@ -16,17 +16,24 @@ class Tree:
 
 	def search(self,node,tree):
 		n = tree
+		r = False
 
 		if n.data == node:
-			return(True)
+			r = True
+			return(r)
 
 		while n.relatives:
-			if node in n.relatives:
-				return(True)
-			for f in n.relatives:
-				self.search(node,f)
 
-		return(False)
+			if node in n.relatives:
+				r = True
+				return(r)
+
+			for f in n.relatives:
+				n = f
+				r = self.search(node,n)
+				return(r)
+
+		return(r)
 
 t = Tree("okay")
 t.add("time")
@@ -35,3 +42,4 @@ t.add("lunch",[0,0,0])
 t.add("breakfast",[0,0,1])
 t.add("dinner",[0,0,2])
 print(t.search("for",t.root))
+print(t.search("aoeu",t.root))
