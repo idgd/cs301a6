db726be46b68936e39e2d44d11463e7064e329c6
idgd <isaac.dudney@icloud.com>
2018-04-06 12:24:40 -0600

    found obvious cases where search doesn't work, fixing

--- Tree.py
+++ Tree.py
@@ -12,11 +12,11 @@ class Tree:
 			n.relatives.insert(index[-1],Node(node))
 		else:
 			self.root.relatives.append(Node(node))
-		
 
 	def search(self,node,tree):
 		n = tree
 		r = False
+		print(n.data, [f.data for f in n.relatives])
 
 		if n.data == node:
 			r = True
@@ -41,5 +41,10 @@ t.add("for",[0,0])
 t.add("lunch",[0,0,0])
 t.add("breakfast",[0,0,1])
 t.add("dinner",[0,0,2])
+print(t.search("okay",t.root))
+print(t.search("time",t.root))
 print(t.search("for",t.root))
+print(t.search("lunch",t.root))
+print(t.search("breakfast",t.root))
+print(t.search("dinner",t.root))
 print(t.search("aoeu",t.root))
