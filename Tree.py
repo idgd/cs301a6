e679772fec4892b1bc9e48369ebc36f905c840e3
idgd <isaac.dudney@icloud.com>
2018-04-06 12:00:37 -0600

    added potentially functional add func, testing search and is broke

--- Tree.py
+++ Tree.py
@@ -4,15 +4,18 @@ class Tree:
 	def __init__(self,root):
 		self.root = Node(root)
 
-	def add(self,node,parent):
-		if search(parent,self):
-			return(True)
+	def add(self,node,index = None):
+		if index:
+			n = self.root
+			for f in index[:-1]:
+				n = n.relatives[f]
+			n.relatives.insert(index[-1],Node(node))
 		else:
-			print("error: parent not found")
-			return(False)
+			self.root.relatives.append(Node(node))
+		
 
 	def search(self,node,tree):
-		n = tree.root
+		n = tree
 
 		if n.data == node:
 			return(True)
@@ -21,6 +24,14 @@ class Tree:
 			if node in n.relatives:
 				return(True)
 			for f in n.relatives:
-				search(node,f)
+				self.search(node,f)
 
 		return(False)
+
+t = Tree("okay")
+t.add("time")
+t.add("for",[0,0])
+t.add("lunch",[0,0,0])
+t.add("breakfast",[0,0,1])
+t.add("dinner",[0,0,2])
+print(t.search("for",t.root))
