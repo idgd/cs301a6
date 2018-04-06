cedb035a573fea7fb45e932b3207f66daf333520
idgd <isaac.dudney@icloud.com>
2018-04-06 11:18:26 -0600

    added more to tree, moved node data to node class

---
+++ Tree.py
@@ -0,0 +1,15 @@
+from Node import Node
+
+class Tree:
+	def __init__(self,root):
+		self.root = Node(root)
+
+	def add(self,node,parent):
+		if search(parent):
+			return(True)
+		else:
+			print("error: parent not found")
+			return(False)
+
+	def search(self,node):
+		return()
