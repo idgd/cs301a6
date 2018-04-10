11e0fbe6de13695f47c47733608fe7a8d7c6a011
idgd <isaac.dudney@icloud.com>
2018-04-10 11:10:16 -0600

    simplified search code

--- Tree.py
+++ Tree.py
@@ -14,33 +14,13 @@ class Tree:
 			self.root.relatives.append(Node(node))
 
 	def search(self,node,tree):
-		r = False
-
 		if tree.data == node:
-			r = True
-			return(r)
+			return(True)
 
-		while tree.relatives:
-			if node in [f.data for f in tree.relatives]:
-				r = True
-				return(r)
-
+		if node in [f.data for f in tree.relatives]:
+			return(True)
+		else:
 			for f in tree.relatives:
-				r = self.search(node,f)
-				return(r)
+				return(self.search(node,f))
 
-		return(r)
-
-t = Tree("okay")
-t.add("time")
-t.add("for",[0,0])
-t.add("lunch",[0,0,0])
-t.add("breakfast",[0,0,1])
-t.add("dinner",[0,0,2])
-print(t.search("okay",t.root))
-print(t.search("time",t.root))
-print(t.search("for",t.root))
-print(t.search("lunch",t.root))
-print(t.search("breakfast",t.root))
-print(t.search("dinner",t.root))
-print(t.search("aoeu",t.root))
+		return(False)
